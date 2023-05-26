c5ed5e3ce449ce83ff72518882ed31b691983777
Bela Bohlender <bbohlender@coconut-capital.com>
2023-05-26 17:39:48 +0200

    setup test

---
+++ test/index.tsx
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom/client";
+import App from "./app.js";
+import React from "react";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
