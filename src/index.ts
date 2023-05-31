e2822deea144279a155e53987bd2b476e81b2422
Bela Bohlender <bbohlender@coconut-capital.com>
2023-05-31 14:35:50 +0200

    remove previous tries

--- src/index.ts
+++ src/index.ts
@@ -50,6 +50,11 @@ export class EventTranslator<E> {
     protected onPointerMissed: () => void
   ) {}
 
+  /**
+   * called when the input device receives a press, release, or move event
+   * @param positionChanged flag to indicate that the input device was moved and therefore requires the **sceneIntersections** to recompute. When the **sceneIntersections** are recomputed, we check whether objects where hovered or released and dispatch events accordingly.
+   * @param pressChanged flag to indicate that any input device element was either pressed or released. Therefore, we check whether the objects in **sceneIntersections** are released or pressed and dispatch events accordingly.
+   */
   update(event: E, positionChanged: boolean, pressChanged: boolean): void {
     //binds the event and the translator (this) to the dispatcher, which are used to create and deliver the events finally
     this.eventDispatcher.bind(event, this);
